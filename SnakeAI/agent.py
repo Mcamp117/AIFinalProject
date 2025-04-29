daa7b28dcb59d87cee66c3647e13b81c66954379
Mcamp117 <mcamp051604@gmail.com>
2025-04-29 12:44:58 -0500

    Added the agent file and AI game file

--- SnakeAI/agent.py
+++ SnakeAI/agent.py
@@ -2,9 +2,9 @@ import torch
 import random
 import numpy as np
 from collections import deque
-from human_game import SnakeGameAI, Direction, Point
+from aiGame import SnakeGameAI, Direction, Point
 from model import Linear_QNet, QTrainer
-from helper import plot
+from lilHelper import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
